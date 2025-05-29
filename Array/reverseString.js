62ad87b3305db7206aaf9007ec1dcee2b175f9eb
THANGTA\tangu <tanguyentruongthang@gmail.com>
2025-05-29 08:08:39 -0700

    minor adjustment

--- Array/reverseString.js
+++ Array/reverseString.js
@@ -1,12 +1,15 @@
 // reverse a string 1st attemp
 const str = "hello";
 function reverseString(str) {
+  if (typeof str != "string" || !str || str.length < 1) {
+    return "Invalid input!";
+  }
   const strArr = str.split("");
   const tempArray = [];
   for (let i = 0; i < strArr.length; i++) {
     tempArray.push(strArr[strArr.length - 1 - i]);
   }
-  return tempArray;
+  return tempArray.join("");
 }
 
 console.log(reverseString(str));
