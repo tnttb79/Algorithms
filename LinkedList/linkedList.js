09ef609958a1eed9dcc1c878f773964923b3a6f5
THANGTA\tangu <tanguyentruongthang@gmail.com>
2025-07-13 16:33:15 -0700

    add prepend() and Node class

--- LinkedList/linkedList.js
+++ LinkedList/linkedList.js
@@ -8,17 +8,27 @@ class LinkedList {
     this.length = 1;
   }
   append(val) {
-    const newNode = {
-      val,
-      next: null,
-    };
+    const newNode = new Node(val);
     this.tail.next = newNode;
     this.tail = newNode;
     this.length++;
+  }
+  prepend(val) {
+    const newNode = new Node(val);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+  }
+}
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
 }
 
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(15);
+myLinkedList.prepend(1);
 console.log(myLinkedList);
