4192b3ccd8d159e5f517bf1e4515fcaae297d333
THANGTA\tangu <tanguyentruongthang@gmail.com>
2025-07-13 23:10:48 -0700

    added a few print methods

--- LinkedList/linkedList.js
+++ LinkedList/linkedList.js
@@ -1,3 +1,5 @@
+//1 - 10 - 5 - 15;
+
 class LinkedList {
   constructor(value) {
     this.head = {
@@ -7,12 +9,47 @@ class LinkedList {
     this.tail = this.head;
     this.length = 1;
   }
+
+  printListWForLoop() {
+    const arrayOfNodes = [];
+    for (
+      let currentNode = this.head;
+      currentNode !== null;
+      currentNode = currentNode.next
+    ) {
+      arrayOfNodes.push(currentNode.value);
+    }
+    console.log(this.listToString(arrayOfNodes));
+  }
+
+  printListWWhileLoop() {
+    let currentNode = this.head;
+    const arrayOfNodes = [];
+    while (currentNode !== null) {
+      arrayOfNodes.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    console.log(this.listToString(arrayOfNodes));
+  }
+
+  listToString(arr) {
+    let stringBuilder = "";
+    arr.forEach((e, i) => {
+      if (i == arr.length - 1) {
+        stringBuilder += e;
+      } else {
+        stringBuilder += e + "-->";
+      }
+    });
+    return stringBuilder;
+  }
   append(val) {
     const newNode = new Node(val);
     this.tail.next = newNode;
     this.tail = newNode;
     this.length++;
   }
+
   prepend(val) {
     const newNode = new Node(val);
     newNode.next = this.head;
@@ -20,6 +57,7 @@ class LinkedList {
     this.length++;
   }
 }
+
 class Node {
   constructor(value) {
     this.value = value;
@@ -31,4 +69,5 @@ const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(15);
 myLinkedList.prepend(1);
-console.log(myLinkedList);
+myLinkedList.printListWForLoop();
+myLinkedList.printListWWhileLoop();
