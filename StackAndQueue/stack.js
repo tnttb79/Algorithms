1bcd3821592fa8bc9f40a0d91dcd892e9310ab68
THANGTA\tangu <tanguyentruongthang@gmail.com>
2025-07-28 11:19:48 -0700

    naive stack =))

---
+++ StackAndQueue/stack.js
@@ -0,0 +1,82 @@
+import { LinkedList, Node } from "../LinkedList/linkedList.js";
+
+// Naive approach for stack, not efficient
+// - pop current uses LinkedList.remove(index) -> require full traversal O(n)
+// - same issue w/ peek() causing O(n) traversal
+class Stack1 {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+  push(value) {
+    if (!this.length) {
+      this.currentStack = new LinkedList(value);
+    } else {
+      this.currentStack.append(value);
+    }
+    this.length++;
+  }
+  pop() {
+    if (this.length == 0) return undefined;
+    const removedItem = this.currentStack.remove(this.length - 1);
+    this.length--;
+    return removedItem;
+  }
+  peek() {
+    return this.currentStack.traverseToIndex(this.length - 1);
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+  push(value) {
+    const newItem = new Node(value);
+    if (!this.length) {
+      this.bottom == this.newItem;
+    } else newItem.next = this.top;
+    this.top = newItem;
+    this.length++;
+  }
+  pop() {}
+  peek() {}
+  isEmpty() {}
+
+  printListWForLoop() {
+    const arrayOfNodes = [];
+    for (
+      let currentNode = this.top;
+      currentNode !== null;
+      currentNode = currentNode.next
+    ) {
+      arrayOfNodes.push(currentNode.value);
+    }
+    console.log(this.listToString(arrayOfNodes));
+  }
+  listToString(arr) {
+    let stringBuilder = "";
+    arr.forEach((e, i) => {
+      if (i == arr.length - 1) {
+        stringBuilder += e;
+      } else {
+        stringBuilder += e + "-->";
+      }
+    });
+    return stringBuilder;
+  }
+}
+
+// Naive Stack
+// const myStack = new Stack1();
+// myStack.push(1);
+// myStack.push(2);
+// myStack.push(3);
+// console.log(myStack.peek());
+// myStack.currentStack.printListWForLoop();
+
+// Giga Chad Stack =))
+const test
