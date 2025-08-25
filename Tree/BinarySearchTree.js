f3170b5843ef28aaaa8ca096c4393ca3aa090a51
THANGTA\tangu <tanguyentruongthang@gmail.com>
2025-08-24 19:09:25 -0700

    added remove a node of BST

--- Tree/BinarySearchTree.js
+++ Tree/BinarySearchTree.js
@@ -52,19 +52,84 @@ class BinarySearchTree {
     return null;
   }
   // remove
+  remove(value) {
+    if (!this.root) {
+      return false;
+    }
+    let currentNode = this.root;
+    let parentNode = null;
+    while (currentNode) {
+      if (value < currentNode.value) {
+        parentNode = currentNode;
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        parentNode = currentNode;
+        currentNode = currentNode.right;
+      } else if (currentNode.value === value) {
+        if (currentNode.right === null) {
+          if (parentNode === null) {
+            this.root = currentNode.left;
+          } else {
+            if (currentNode.value < parentNode.value) {
+              parentNode.left = currentNode.left;
+            } else if (currentNode.value > parentNode.value) {
+              parentNode.right = currentNode.left;
+            }
+          }
+        } else if (currentNode.right.left === null) {
+          currentNode.right.left = currentNode.left;
+          if (parentNode === null) {
+            this.root = currentNode.right;
+          } else {
+            if (currentNode.value < parentNode.value) {
+              parentNode.left = currentNode.right;
+            } else if (currentNode.value > parentNode.value) {
+              parentNode.right = currentNode.right;
+            }
+          }
+        } else {
+          let leftmost = currentNode.right.left;
+          let leftmostParent = currentNode.right;
+          while (leftmost.left !== null) {
+            leftmostParent = leftmost;
+            leftmost = leftmost.left;
+          }
+
+          leftmostParent.left = leftmost.right;
+          leftmost.left = currentNode.left;
+          leftmost.right = currentNode.right;
+
+          if (parentNode === null) {
+            this.root = leftmost;
+          } else {
+            if (currentNode.value < parentNode.value) {
+              parentNode.left = leftmost;
+            } else if (currentNode.value > parentNode.value) {
+              parentNode.right = leftmost;
+            }
+          }
+        }
+        return true;
+      }
+    }
+    return false;
+  }
 }
 
 const tree = new BinarySearchTree();
 tree.insert(9);
+
 tree.insert(4);
 tree.insert(6);
 tree.insert(20);
 tree.insert(170);
 tree.insert(15);
 tree.insert(1);
-// JSON.stringify(traverse(tree.root))
+console.log(JSON.stringify(traverse(tree.root)));
 tree.lookup(15);
 tree.lookup(7);
+console.log("remvoved 7" + tree.remove(7));
+console.log("remvoved 15" + tree.remove(15));
 
 //     9
 //  4     20
