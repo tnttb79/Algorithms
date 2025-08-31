54d28d7158a21a3177283d2c8e955f832f390be1
THANGTA\tangu <tanguyentruongthang@gmail.com>
2025-08-30 22:00:40 -0700

    another fibon???? Really?

--- Miscellanious/recursion.js
+++ Miscellanious/recursion.js
@@ -1,19 +1,19 @@
-function factorialRecursive(number) {
-  if (number == 1) return 1;
-  return number * factorialRecursive(number - 1);
-}
+// function factorialRecursive(number) {
+//   if (number == 1) return 1;
+//   return number * factorialRecursive(number - 1);
+// }
 
-const factorialLoop = (number) => {
-  let result = number;
-  while (number !== 1) {
-    result = result * (number - 1);
-    number--;
-  }
-  return result;
-};
+// const factorialLoop = (number) => {
+//   let result = number;
+//   while (number !== 1) {
+//     result = result * (number - 1);
+//     number--;
+//   }
+//   return result;
+// };
 
-console.log(factorialRecursive(5));
-console.log(factorialLoop(5));
+// console.log(factorialRecursive(5));
+// console.log(factorialLoop(5));
 
 const fibonacciLoop = (n) => {
   let result = [];
@@ -26,4 +26,21 @@ const fibonacciLoop = (n) => {
   return result;
 };
 
-console.log(fibonacciR(5));
+console.log(fibonacciLoop(5));
+
+function fibonacciLoop1(n) {
+  if (n < 2) return n;
+  else {
+    let previous = 0;
+    let current = 1;
+    let next = 1;
+    for (let i = 2; i <= n; i++) {
+      previous = current;
+      current = next;
+      next = current + previous;
+    }
+    return current;
+  }
+}
+
+console.log(fibonacciLoop1(9));
