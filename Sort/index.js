5360b61b90971744e45801bf5cc9b21ae6badc99
THANGTA\tangu <tanguyentruongthang@gmail.com>
2025-09-01 00:03:15 -0700

    2 approaches for insertion sort

--- Sort/index.js
+++ Sort/index.js
@@ -16,7 +16,7 @@ const bubbleSort = (array) => {
   return array;
 };
 
-console.log(bubbleSort(arr));
+// console.log(bubbleSort(arr));
 
 const selectionSort = (array) => {
   for (let i = 0; i < array.length; i++) {
@@ -30,3 +30,51 @@ const selectionSort = (array) => {
   return arr;
 };
 // console.log(selectionSort(arr));
+
+const insertionSort = (array) => {
+  let result = [array[0]];
+  for (let i = 1; i < array.length; i++) {
+    for (let j = 0; j < result.length; j++) {
+      if (array[i] < result[j]) {
+        result.splice(j, 0, array[i]);
+        break;
+      }
+      if (j == result.length - 1) {
+        result.push(array[i]);
+        break;
+      }
+    }
+  }
+  return result;
+};
+// console.log(insertionSort(arr));
+
+// const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+// function insertionSort1(array) {
+//   const length = array.length;
+
+//   for (let i = 1; i < length; i++) {
+//     5;
+//     if (array[i] < array[0]) {
+//       // Move number to the first position
+//       array.unshift(array.splice(i, 1)[0]);
+//     } else {
+//       // Find where number should go
+//       for (let j = 1; j < i; j++) {
+//         // if (array[i] > array[j - 1] && array[i] < array[j]) {
+//         //   // Move number to the right spot
+//         //   array.splice(j, 0, array.splice(i, 1)[0]);
+//         // }
+//         if (array[j] > array[i]) {
+//           array.splice(j, 0, array.splice(i, 1)[0]);
+//           break;
+//         }
+//       }
+//     }
+//   }
+
+//   return array;
+// }
+
+// console.log(insertionSort1(numbers));
