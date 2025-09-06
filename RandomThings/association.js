6f907cd31d5db04b9198b7695a70592246c35a5d
THANGTA\tangu <tanguyentruongthang@gmail.com>
2025-09-06 16:53:01 -0700

    private is not truly private after all, it's still accessible in runtime

--- RandomThings/association.js
+++ RandomThings/association.js
@@ -1,22 +1,52 @@
+// class Driver {
+//   private name: string;
+//   /**
+//    *
+//    */
+//   constructor(name: string) {
+//     this.name = name;
+//   }
+//   getName = (): string => {
+//     return this.name;
+//   };
+// }
+var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, state, value, kind, f) {
+    if (kind === "m") throw new TypeError("Private method is not writable");
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a setter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
+    return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
+};
+var _Driver_name;
+// class Car {
+//   /**
+//    *
+//    */
+//   private driver: Driver;
+//   constructor(driver: Driver) {
+//     this.driver = driver;
+//   }
+//   drive(): void {
+//     console.log("car is driving by:" + this.driver.getName());
+//   }
+// }
+// const driver = new Driver("Thang");
+// const car = new Car(driver);
+// car.drive();
 var Driver = /** @class */ (function () {
-    /**
-     *
-     */
     function Driver(name) {
-        var _this = this;
-        this.getName = function () {
-            return _this.name;
-        };
-        this.name = name;
+        _Driver_name.set(this, void 0);
+        __classPrivateFieldSet(this, _Driver_name, name, "f");
     }
     return Driver;
 }());
+_Driver_name = new WeakMap();
 var Car = /** @class */ (function () {
     function Car(driver) {
         this.driver = driver;
     }
     Car.prototype.drive = function () {
-        console.log("car is driving by:" + this.driver.getName());
+        var test = "name";
+        console.log("".concat(this.driver[test], " is driving the car."));
     };
     return Car;
 }());
--- RandomThings/association.ts
+++ RandomThings/association.ts
@@ -1,26 +1,51 @@
+// class Driver {
+//   private name: string;
+//   /**
+//    *
+//    */
+//   constructor(name: string) {
+//     this.name = name;
+//   }
+//   getName = (): string => {
+//     return this.name;
+//   };
+// }
+
+// class Car {
+//   /**
+//    *
+//    */
+//   private driver: Driver;
+//   constructor(driver: Driver) {
+//     this.driver = driver;
+//   }
+//   drive(): void {
+//     console.log("car is driving by:" + this.driver.getName());
+//   }
+// }
+
+// const driver = new Driver("Thang");
+// const car = new Car(driver);
+// car.drive();
+
 class Driver {
   private name: string;
-  /**
-   *
-   */
+
   constructor(name: string) {
     this.name = name;
   }
-  getName = (): string => {
-    return this.name;
-  };
 }
 
 class Car {
-  /**
-   *
-   */
-  private driver: Driver;
+  private driver: Driver; // Car has-a Driver
+
   constructor(driver: Driver) {
     this.driver = driver;
   }
+
   drive(): void {
-    console.log("car is driving by:" + this.driver.getName());
+    const test = "name";
+    console.log(`${this.driver[test]} is driving the car.`);
   }
 }
 
