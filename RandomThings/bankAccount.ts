de9cfcbf1b22e6007b2e4769ee87af8a67b5372c
THANGTA\tangu <tanguyentruongthang@gmail.com>
2025-09-06 13:34:35 -0700

    testing some encapsulation with typescript

---
+++ RandomThings/bankAccount.ts
@@ -0,0 +1,19 @@
+class BankAccount {
+  /**
+   *
+   */
+  private balance: number = 0.0;
+  constructor() {}
+  Deposit(ammount: number): number {
+    if (ammount < 0) throw new Error("Deposit amount can't be negative");
+    else this.balance += ammount;
+
+    return this.balance;
+  }
+  WithDraw(ammount: number): number {
+    if (ammount < 0) throw new Error("WithDraw amount can't be negative");
+    else this.balance -= ammount;
+    return this.balance;
+  }
+  GetBalance = (): number => this.balance;
+}
