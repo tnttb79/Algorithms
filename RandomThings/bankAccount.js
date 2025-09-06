2565bfa88b726797062b8279d7762c992c991b4b
THANGTA\tangu <tanguyentruongthang@gmail.com>
2025-09-06 13:36:07 -0700

    compiled js

--- RandomThings/bankAccount.js
+++ RandomThings/bankAccount.js
@@ -1,4 +1,25 @@
-var greet = function () {
-  console.log("hi");
-};
-greet();
+var BankAccount = /** @class */ (function () {
+    function BankAccount() {
+        var _this = this;
+        /**
+         *
+         */
+        this.balance = 0.0;
+        this.GetBalance = function () { return _this.balance; };
+    }
+    BankAccount.prototype.Deposit = function (ammount) {
+        if (ammount < 0)
+            throw new Error("Deposit amount can't be negative");
+        else
+            this.balance += ammount;
+        return this.balance;
+    };
+    BankAccount.prototype.WithDraw = function (ammount) {
+        if (ammount < 0)
+            throw new Error("WithDraw amount can't be negative");
+        else
+            this.balance -= ammount;
+        return this.balance;
+    };
+    return BankAccount;
+}());
