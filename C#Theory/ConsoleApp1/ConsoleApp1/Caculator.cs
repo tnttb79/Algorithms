1a6bf1fe082212e3c706ea6848274049d5a6dd24
THANGTA\tangu <tanguyentruongthang@gmail.com>
2025-09-07 11:25:49 -0700

    over engineer calculator lol

---
+++ C#Theory/ConsoleApp1/ConsoleApp1/Caculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    interface IOperation
+    {
+        double Calculate(double number1, double number2);
+    }
+
+    class Add : IOperation
+    {
+        public double Calculate(double number1, double number2)
+        {
+            return number1 + number2;
+        }
+    }
+    class Subtract : IOperation 
+    {
+        public double Calculate(double number1, double number2)
+        {
+            return number1 - number2;
+        }
+    }
+    class Caculator
+    {
+        public double Calculate(IOperation operation, double number1, double number2)
+        {
+            return operation.Calculate(number1, number2);
+        }
+
+    }
+
+}
