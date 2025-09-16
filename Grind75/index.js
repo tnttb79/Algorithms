05fa2c48fa8aa642c63f45157ef33ebc1dc31ce2
THANGTA\tangu <tanguyentruongthang@gmail.com>
2025-09-15 17:58:10 -0700

    merge 2 sorted linkedLists

--- Grind75/index.js
+++ Grind75/index.js
@@ -19,3 +19,20 @@ var twoSum = function (nums, target) {
     hashmap[current] = i;
   }
 };
+
+// Merge Two Sorted Lists: O(m+n)
+var mergeTwoLists = function (list1, list2) {
+  tempHead = new ListNode();
+  current = tempHead;
+  while (list1 && list2) {
+    if (list1.val > list2.val) {
+      current.next = list2;
+      list2 = list2.next;
+    } else {
+      current.next = list1;
+      list1 = list1.next;
+    }
+    current = current.next;
+  }
+  return tempHead.next;
+};
