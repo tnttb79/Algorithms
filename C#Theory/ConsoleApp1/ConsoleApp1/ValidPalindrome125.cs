6b2420d58f9f6fcd74a9a0be151d18e2573edf33
THANGTA\tangu <tanguyentruongthang@gmail.com>
2025-10-06 23:19:28 -0700

    valid palindrome

---
+++ C#Theory/ConsoleApp1/ConsoleApp1/ValidPalindrome125.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class ValidPalindrome125
+    {
+        public bool IsPalindrome(string s)
+        {
+            string normalized = string.Empty;
+           foreach(char letter in s)
+            {
+                if (char.IsLetterOrDigit(letter)) normalized += letter; 
+            }
+            normalized = normalized.ToLower();
+            int left = 0;
+            int right = normalized.Length - 1;
+            while(left < right)
+            {
+                if (normalized[left] != normalized[right]) return false;
+            }
+            return true;
+        }
+    }
+}
--- Grind75/index.js
+++ Grind75/index.js
@@ -76,3 +76,16 @@ var maxProfit = function (prices) {
   }
   return max;
 };
+
+// 125. valid palindrome
+var isPalindrome = function (s) {
+  const normalized = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+  let left = 0;
+  let right = normalized.length - 1;
+  while (left < right) {
+    if (normalized[left] !== normalized[right]) return false;
+    left++;
+    right--;
+  }
+  return true;
+};
