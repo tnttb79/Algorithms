40abdaeebf654b2a894adc7e00a42dfddac3fb8f
THANGTA\tangu <tanguyentruongthang@gmail.com>
2025-10-06 23:18:59 -0700

    2 sum

---
+++ C#Theory/ConsoleApp1/ConsoleApp1/TwoSum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class TwoSumSolution
+    {
+        public string someRandom {  get; set; }
+        public int someRandomInt { get; set; }
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> dict = new();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var numToFind = target - nums[i];
+                if (dict.ContainsKey(numToFind)) return [dict[numToFind], i];
+                else dict[numToFind] = i;
+            }
+            return [];
+        }
+    }
+}
