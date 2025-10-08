9c5f88c9c548f60f7bc4ff8257c2e29f3927e384
THANGTA\tangu <tanguyentruongthang@gmail.com>
2025-10-07 23:14:44 -0700

    226 invert binary tree

---
+++ C#Theory/ConsoleApp1/ConsoleApp1/InvertBinaryTree226.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class InvertBinaryTree226
+    {
+
+    }
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
