6a5fccc11bd152111f953bd170f6baee63fc38f0
THANGTA\tangu <tanguyentruongthang@gmail.com>
2025-10-20 23:14:34 -0700

    binary search c#

---
+++ C#Theory/ConsoleApp1/ConsoleApp1/BinarySearch704.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class BinarySearch704
+    {
+        public int Search(int[] nums, int target)
+        {
+            int low = 0, high = nums.Length - 1;
+            while (low < high)
+            {
+                int mid = low + (int)Math.Floor(((double)high -low +1)/2);
+                if (target < nums[mid]) high = mid - 1;
+                else low = mid;
+            }
+            return nums[low] == target ? low : -1;
+        }
+    }
+}
