3b73c074f42f11a53a0a1b6f012a66485df4e2e5
THANGTA\tangu <tanguyentruongthang@gmail.com>
2025-10-20 23:14:22 -0700

    binary search javascript

--- Grind75/index.js
+++ Grind75/index.js
@@ -112,3 +112,15 @@ var isAnagram = function (s, t) {
   }
   return true;
 };
+
+// 704. Binary search:
+var search = function (nums, target) {
+  let low = 0;
+  high = nums.length - 1;
+  while (low != high) {
+    let mid = low + Math.floor((high - low + 1) / 2);
+    if (target > nums[mid]) low = mid + 1;
+    else high = mid;
+  }
+  return nums[low] == target ? 1 : 0;
+};
